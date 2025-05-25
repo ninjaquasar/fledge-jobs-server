35825b3e9b616a80bb0aa9aafb16b8cf86b9fabf
mahnaf <mahnaf125@gmail.com>
2025-05-25 23:05:42 +0600

    feat(jobs): add expertise_level and pricing_type field in job model

--- job/models.py
+++ job/models.py
@@ -11,14 +11,36 @@ class Job(models.Model):
     ONSITE = 'ON-SITE'
     REMOTE = 'REMOTE'
     HYBRID = 'HYBRID'
+    ENTRY_LEVEL = 'ENTRY-LEVEL'
+    JUNIOR = 'JUNIOR'
+    INTERMEDIATE = 'INTERMEDIATE'
+    EXPERT = 'EXPERT'
+    FIXED = 'FIXED'
+    CONTRACTUAL = 'CONTRACTUAL'
+    HOURLY = 'HOURLY'
+    PROJECT_BASED = 'PROJECT-BASED'
     LOCATION_TYPE_CHOICES = (
         (ONSITE, "On-Site"),
         (REMOTE, "Remote"),
         (HYBRID, "Hybrid"),
     )
+    EXPERTISE_LEVEL_CHOICES = (
+        (ENTRY_LEVEL, "Entry-Level"),
+        (JUNIOR, "Junior"),
+        (INTERMEDIATE, "Intermediate"),
+        (EXPERT, "Expert"),
+    )
+    PRICING_TYPE_CHOICES = (
+        (FIXED, "Fixed"),
+        (CONTRACTUAL, "Contractual"),
+        (HOURLY, "Hourly"),
+        (PROJECT_BASED, "Project-based"),
+    )
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
     company_name = models.CharField(max_length=75)
+    expertise_level = models.CharField(max_length=12, choices=EXPERTISE_LEVEL_CHOICES)
+    pricing_type = models.CharField(max_length=13, choices=PRICING_TYPE_CHOICES)
     requirements = models.CharField(max_length=150)
     location = models.CharField(max_length=75)
     location_type = models.CharField(max_length=7, choices=LOCATION_TYPE_CHOICES)
