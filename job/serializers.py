f24a64951d43c3306e64f665fecc4e98b56f1db7
mahnaf <mahnaf125@gmail.com>
2025-05-25 23:23:06 +0600

    add serializer fields for latest added model fields

--- job/serializers.py
+++ job/serializers.py
@@ -3,9 +3,10 @@ from job.models import Job, Category
 
 
 class JobSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
     class Meta:
         model = Job
-        fields = ['title', 'company_name', 'description', 'requirements', 'location', 'location_type', 'date_posted', 'category']
+        fields = ['title', 'company_name', 'description', 'requirements', 'location', 'location_type', 'expertise_level', 'pricing_type', 'category', 'date_posted']
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
